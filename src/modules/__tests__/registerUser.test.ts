f9e7114c2a2dd77bc55df9df7ded9ed9540e6af8
tyrell-snyders <tyrell.snyders@gmail.com>
2023-09-29 13:23:46 +0200

    test: mock user

--- src/modules/__tests__/registerUser.test.ts
+++ src/modules/__tests__/registerUser.test.ts
@@ -1,12 +1,21 @@
 import { describe, it, vi, expect } from 'vitest'
 import { createServer } from '../../utils/createServer'
 import userService from '../services/user.service'
+import UserModel from '../model/user.model'
 
 describe('Post "/api/auth/register" route', () => {
     it('should call the registerUser service', async () => {
 
+        let user: UserModel = {
+            username: "mock username",
+            email: "mock email",
+            pass: "mock pass"
+        }  as UserModel
+
         const registerUserSpy = vi.spyOn(userService, 'registerUser')
         expect(registerUserSpy.getMockName()).toEqual('registerUser')
+
+        registerUserSpy.mockResolvedValue(user)
 
         const server = await createServer()
         await server.ready()
